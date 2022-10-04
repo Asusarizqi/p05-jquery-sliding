9913e47869aed617dceacb8d5aaa0d2f69f9d1c7
Asusarizqi <112870030+Asusarizqi@users.noreply.github.com>
2022-10-05 03:22:29 +0700

    Add files via upload

---
+++ Script-sliding.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".karet1").click(function(){
+        $(".isi1").slideToggle("slow");
+    });
+
+    $(".karet2").click(function(){
+        $(".isi2").slideToggle("slow");
+    });
+});
